0598cba0385e09db9cfe0bb1a1e816f36afa1274
gregspullen <gregpullen21@googlemail.com>
2026-04-03 00:24:44 +0100

    Update assembler.py

--- assembler.py
+++ assembler.py
@@ -75,11 +75,9 @@ while True:
 
     confirm = input("\nIs this correct? (y/n): ").lower()
 
-    if confirm == "y":
-        
-    print("🔥 CALLING UPDATE FUNCTION")
-    update_data_js(data["slug"], title, data["meta_description"])
-        
+    if confirm == "y":    
+        print("🔥 CALLING UPDATE FUNCTION")
+        update_data_js(data["slug"], title, data["meta_description"])    
         break
 
     print("\nLet’s try again...\n")
