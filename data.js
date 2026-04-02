0f7f0ff1ac960c547f29ec3a5cf168d8c597a2b5
gregspullen <gregpullen21@googlemail.com>
2026-04-03 00:41:49 +0100

    Add files via upload

--- data.js
+++ data.js
@@ -1,2 +1,10 @@
 const PIECES = {
+  "lavenders-blue": {
+    "title": "Lavender's Blue",
+    "description": "English folk song. SATB choir and piano."
+  },
+  "bathed-in-starlight": {
+    "title": "Bathed In Starlight",
+    "description": "Nativity Carol. SSA or unison voices."
+  }
 };
