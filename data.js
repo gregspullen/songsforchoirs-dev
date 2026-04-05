821171bf3bda39edd982f9b64fe01e627b344b09
gregspullen <gregpullen21@googlemail.com>
2026-04-05 09:59:50 +0100

    Add files via upload

--- data.js
+++ data.js
@@ -1,20 +1,20 @@
 window.PIECES = {
   "lavenders-blue": {
     "title": "Lavender's Blue",
-    "metaTitle": "Lavender's Blue - for SATB Choir with Piano | Songs for Choirs",
-    "description": "Hmmm smelly",
+    "metaTitle": "Lavender's Blue - for SATB Choir with optional piano accompniment | Songs for Choirs",
+    "description": "Lavenders Blue  a light and humorous arrangement of the traditional folk song for SATB choir. Fun, accessible, and full of charm, it makes an ideal short concert item. View the score, listen, and download licensed copies from Songs for Choirs.",
     "hasScore": true
   },
   "bathed-in-starlight": {
     "title": "Bathed In Starlight",
-    "metaTitle": "Bathed In Starlight - Nativity Carol for SSA or Unison Choir | Songs for Choirs",
-    "description": "Turn that light off!",
+    "metaTitle": "Bathed In Starlight - Nativity Carol for SSA or Unison Choir and piano | Songs for Choirs",
+    "description": "Bathed in Starlight  a beautiful and expressive narrative choral work for young voices. For SSA or flexible two-part and unison choir, it moves from lyrical, flowing lines to rhythmic, percussive energy. View the score, listen, and download licensed copies from Songs for Choirs.",
     "hasScore": true
   },
-  "froglets": {
-    "title": "Froglets",
-    "metaTitle": "Froglets  Very silly name | Songs for Choirs",
-    "description": "hop hop hop",
+  "away-in-a-manger": {
+    "title": "Away in a Manger",
+    "metaTitle": "Away in a Manger - Nativity Carol for SATB with optional piano accompaniment | Songs for Choirs",
+    "description": "Away in a Manger  a fresh and inventive choral setting combining two traditional melodies. Originally conceived a cappella, with optional piano support. A lively concert piece for SATB choir with rich and rewarding part-writing. View the score, listen, and download licensed copies from Songs for Choirs.",
     "hasScore": false
   }
 };
