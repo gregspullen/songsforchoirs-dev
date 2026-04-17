a04890e1c8b89624f317b1d6d27e2baec9c1b884
gregspullen <gregpullen21@googlemail.com>
2026-04-17 12:06:48 +0100

    Update data.js

--- data.js
+++ data.js
@@ -6,6 +6,13 @@ window.PIECES = {
     "hasScore": true,
     "categories": ["choral"]
   },
+  "lavenders-blue": {
+    "title": "Lavender's Blue",
+    "description": "Lavenders Blue - a light and humorous arrangement of the traditional folk song for SATB choir. Fun, accessible, and full of charm, it makes an ideal short concert item. View the score, listen, and download licensed copies from Songs for Choirs.",",
+    "categories": ["choral"],
+    "voicing": "SATB",
+    "accompaniment": "piano (optional)"
+  },
   "bathed-in-starlight": {
     "title": "Bathed in Starlight",
     "voicing": "SSA",
