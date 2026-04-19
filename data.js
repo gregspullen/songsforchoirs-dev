f0dccf82bb94b7f9e53d28bff48a66187b1d1239
gregspullen <gregpullen21@googlemail.com>
2026-04-20 00:04:13 +0100

    Update data.js

--- data.js
+++ data.js
@@ -127,7 +127,7 @@ window.PIECES = {
     "price": 1.00
   },
   "make-a-joyful-noise": {
-    "title": "Venite",
+    "title": "Make a Joyful Noise",
     "description": "Make a Joyful Noise - a joyful and rhythmic choral setting for SATB choir, written in a similar style to the Magnificat. Energetic, engaging, and accessible. View the score, listen, and download licensed copies from Songs for Choirs.",    
     "voicing": "SATB",
     "accompaniment": "piano",
