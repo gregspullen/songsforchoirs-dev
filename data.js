db81b5a23960ebcc803dfa9c2ad7813c19edbed5
gregspullen <gregpullen21@googlemail.com>
2026-04-19 10:59:31 +0100

    Update data.js

--- data.js
+++ data.js
@@ -6,7 +6,7 @@ window.PIECES = {
     "accompaniment": "a cappella",
     "categories": ["choral"],
     "hasScore": true,
-    "price": 1.02
+    "price": 1.10
   },
   "lavenders-blue": {
     "title": "Lavender's Blue",
